ada59b8c978d20c5c48e0f76b9e07df4f0624435
Selim Ozkan <selimozkan@gmail.com>
2024-09-19 11:13:00 +0300

    First Model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class General(models.Model):
+    header_text_en = models.CharField(max_length=150, null=True, blank=True)
+    header_text_ge = models.CharField(max_length=150, null=True, blank=True)
+    banner_image = models.ImageField(upload_to="general/", null=True, blank=True)
+    banner_title_en = models.CharField(max_length=150, null=True, blank=True)
+    banner_title_ge = models.CharField(max_length=150, null=True, blank=True)
+    description_welcome_en = models.TextField(null=True, blank=True)
+    description_welcome_ge = models.TextField(null=True, blank=True)
+    project_title_en = models.CharField(max_length=100, null=True, blank=True)
+    project_title_ge = models.CharField(max_length=100, null=True, blank=True)
+    project_description_en = models.TextField(null=True, blank=True)
+    project_description_ge = models.TextField(null=True, blank=True)
+    email = models.EmailField(max_length=254, null=True, blank=True)
